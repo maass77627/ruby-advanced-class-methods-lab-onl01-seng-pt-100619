6a7420d61558e2154897eabe74183aad119db648
kelly r maass <kmaass77627@gmail.com>
2019-11-07 04:00:49 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -36,7 +36,7 @@ class Song
 def self.find_or_create_by_name(name)
       song.find_by_name(name)
       else 
-        song.create_by_name(songs)
+        song.create_by_name(name)
       end
     end
   
